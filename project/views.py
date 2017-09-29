6acfecc663d42e3be723b4d6b02c0e42a64bdc74
IX54 <christopheryin@hotmail.com>
2017-09-29 19:05:43 +1000

    Add files via upload

---
+++ project/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'project/home.html')
+
+def about(request):
+    return render(request, 'project/about.html') #, {'content':['Test', 'Test']}
