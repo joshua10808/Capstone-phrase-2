451dce6a0122b5634a1be1b56d25cd0ffb5adb15
IX54 <christopheryin@hotmail.com>
2017-10-21 16:40:06 +1000

    Add files via upload

--- Group-74 Capstone Project/capstone/job_search/views.py
+++ Group-74 Capstone Project/capstone/job_search/views.py
@@ -4,3 +4,105 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def job_search_unfiltered(request):
+    return render(request, 'job_search/job_search_unfiltered.html')
+
+def job_search_informationsystems_major(request):
+    return render(request, 'job_search/job_search_informationsystems_major.html')
+
+def job_search_computerscience_major(request):
+    return render(request, 'job_search/job_search_computerscience_major.html')
+
+def job_search_computationalandsimsci_major(request):
+    return render(request, 'job_search/job_search_computationalandsimsci_major.html')
+
+def job_search_technologyinnovationanddesign_major(request):
+    return render(request, 'job_search/job_search_technologyinnovationanddesign_major.html')
+
+def job_search_bpm(request):
+    return render(request, 'job_search/job_search_bpm.html')
+
+def job_search_computationalandsimsci_minor(request):
+    return render(request, 'job_search/job_search_computationalandsimsci_minor.html')
+
+def job_search_computerscience_minor(request):
+    return render(request, 'job_search/job_search_computerscience_minor.html')
+
+def job_search_datacentriccomputingextension(request):
+    return render(request, 'job_search/job_search_datacentriccomputingextension.html')
+
+def job_search_enterprisesystems(request):
+    return render(request, 'job_search/job_search_enterprisesystems.html')
+
+def job_search_intelligentsystems(request):
+    return render(request, 'job_search/job_search_intelligentsystems.html')
+
+def job_search_mobileapps(request):
+    return render(request, 'job_search/job_search_mobileapps.html')
+
+def job_search_networkandsecurity(request):
+    return render(request, 'job_search/job_search_networkandsecurity.html')
+
+def job_search_softwaredevelopmentforis(request):
+    return render(request, 'job_search/job_search_softwaredevelopmentforis.html')
+
+def job_search_technologyinnovation(request):
+    return render(request, 'job_search/job_search_technologyinnovation.html')
+
+def job_search_userexperience(request):
+    return render(request, 'job_search/job_search_userexperience.html')
+
+def job_search_analysis_skill(request):
+    return render(request, 'job_search/job_search_analysis_skill.html')
+
+def job_search_artificialintelligience_skill(request):
+    return render(request, 'job_search/job_search_artificialintelligience_skill.html')
+
+def job_search_cloudcomputing_skill(request):
+    return render(request, 'job_search/job_search_cloudcomputing_skill.html')
+
+def job_search_database_skill(request):
+    return render(request, 'job_search/job_search_database_skill.html')
+
+def job_search_design_skill(request):
+    return render(request, 'job_search/job_search_design_skill.html')
+
+def job_search_enterprisearchitecture_skill(request):
+    return render(request, 'job_search/job_search_enterprisearchitecture_skill.html')
+
+def job_search_gamedevelopment_skill(request):
+    return render(request, 'job_search/job_search_gamedevelopment_skill.html')
+
+def job_search_hardware_skill(request):
+    return render(request, 'job_search/job_search_hardware_skill.html')
+
+def job_search_itconsulting_skill(request):
+    return render(request, 'job_search/job_search_itconsulting_skill.html')
+
+def job_search_itfundamentals_skill(request):
+    return render(request, 'job_search/job_search_itfundamentals_skill.html')
+
+def job_search_modelling_skill(request):
+    return render(request, 'job_search/job_search_modelling_skill.html')
+
+def job_search_networking_skill(request):
+    return render(request, 'job_search/job_search_networking_skill.html')
+
+def job_search_programming_skill(request):
+    return render(request, 'job_search/job_search_programming_skill.html')
+
+def job_search_projectmanagement_skill(request):
+    return render(request, 'job_search/job_search_projectmanagement_skill.html')
+
+def job_search_security_skill(request):
+    return render(request, 'job_search/job_search_security_skill.html')
+
+def job_search_socialmedia_skill(request):
+    return render(request, 'job_search/job_search_socialmedia_skill.html')
+
+def job_search_systemadministration_skill(request):
+    return render(request, 'job_search/job_search_systemadministration_skill.html')
+
+def job_search_webdevelopment_skill(request):
+    return render(request, 'job_search/job_search_webdevelopment_skill.html')
