2993731b4089f997e150c37488dceff55347101e
IX54 <christopheryin@hotmail.com>
2017-10-24 19:02:28 +1000

    Add files via upload

--- Group-74 Capstone Project/capstone/employer_search/urls.py
+++ Group-74 Capstone Project/capstone/employer_search/urls.py
@@ -16,9 +16,39 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from employer_search.models import Employer_Post
+from . import views
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Employer_Post.objects.all().order_by("-date")[:25],
                                 template_name="employer_search/employer_search.html")),
-    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Employer_Post, template_name = 'employer_search/employer_post.html'))
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Employer_Post, template_name = 'employer_search/employer_post.html')),
+    url(r'^unfiltered/$', views.employer_search_unfiltered, name = "employer_search_unfiltered"),
+    url(r'^0/$', views.employer_search_a, name = "employer_search_0" ),
+    url(r'^a/$', views.employer_search_a, name = "employer_search_a" ),
+    url(r'^b/$', views.employer_search_a, name = "employer_search_b" ),
+    url(r'^c/$', views.employer_search_a, name = "employer_search_c" ),
+    url(r'^d/$', views.employer_search_a, name = "employer_search_d" ),
+    url(r'^e/$', views.employer_search_a, name = "employer_search_e" ),
+    url(r'^f/$', views.employer_search_a, name = "employer_search_f" ),
+    url(r'^g/$', views.employer_search_a, name = "employer_search_g" ),
+    url(r'^h/$', views.employer_search_a, name = "employer_search_h" ),
+    url(r'^i/$', views.employer_search_a, name = "employer_search_i" ),
+    url(r'^j/$', views.employer_search_a, name = "employer_search_j" ),
+    url(r'^k/$', views.employer_search_a, name = "employer_search_k" ),
+    url(r'^l/$', views.employer_search_a, name = "employer_search_l" ),
+    url(r'^m/$', views.employer_search_a, name = "employer_search_m" ),
+    url(r'^n/$', views.employer_search_a, name = "employer_search_n" ),
+    url(r'^o/$', views.employer_search_a, name = "employer_search_o" ),
+    url(r'^p/$', views.employer_search_a, name = "employer_search_p" ),
+    url(r'^q/$', views.employer_search_a, name = "employer_search_q" ),
+    url(r'^r/$', views.employer_search_a, name = "employer_search_r" ),
+    url(r'^s/$', views.employer_search_a, name = "employer_search_s" ),
+    url(r'^t/$', views.employer_search_a, name = "employer_search_t" ),
+    url(r'^u/$', views.employer_search_a, name = "employer_search_u" ),
+    url(r'^v/$', views.employer_search_a, name = "employer_search_v" ),
+    url(r'^w/$', views.employer_search_a, name = "employer_search_w" ),
+    url(r'^x/$', views.employer_search_a, name = "employer_search_x" ),
+    url(r'^y/$', views.employer_search_a, name = "employer_search_y" ),
+    url(r'^z/$', views.employer_search_a, name = "employer_search_z" ),
+    
 ]
--- Group-74 Capstone Project/capstone/employer_search/views.py
+++ Group-74 Capstone Project/capstone/employer_search/views.py
@@ -4,3 +4,87 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def employer_search_unfiltered(request):
+    return render(request, 'employer_search/employer_search_unfiltered.html')
+
+def employer_search_0(request):
+    return render(request, 'employer_search/employer_search_0.html')
+
+def employer_search_a(request):
+    return render(request, 'employer_search/employer_search_a.html')
+
+def employer_search_b(request):
+    return render(request, 'employer_search/employer_search_b.html')
+
+def employer_search_c(request):
+    return render(request, 'employer_search/employer_search_c.html')
+
+def employer_search_d(request):
+    return render(request, 'employer_search/employer_search_d.html')
+
+def employer_search_e(request):
+    return render(request, 'employer_search/employer_search_e.html')
+
+def employer_search_f(request):
+    return render(request, 'employer_search/employer_search_f.html')
+
+def employer_search_g(request):
+    return render(request, 'employer_search/employer_search_g.html')
+
+def employer_search_h(request):
+    return render(request, 'employer_search/employer_search_h.html')
+
+def employer_search_i(request):
+    return render(request, 'employer_search/employer_search_i.html')
+
+def employer_search_j(request):
+    return render(request, 'employer_search/employer_search_j.html')
+
+def employer_search_k(request):
+    return render(request, 'employer_search/employer_search_k.html')
+
+def employer_search_l(request):
+    return render(request, 'employer_search/employer_search_l.html')
+
+def employer_search_m(request):
+    return render(request, 'employer_search/employer_search_m.html')
+
+def employer_search_n(request):
+    return render(request, 'employer_search/employer_search_n.html')
+
+def employer_search_o(request):
+    return render(request, 'employer_search/employer_search_o.html')
+
+def employer_search_p(request):
+    return render(request, 'employer_search/employer_search_p.html')
+
+def employer_search_q(request):
+    return render(request, 'employer_search/employer_search_q.html')
+
+def employer_search_r(request):
+    return render(request, 'employer_search/employer_search_r.html')
+
+def employer_search_s(request):
+    return render(request, 'employer_search/employer_search_s.html')
+
+def employer_search_t(request):
+    return render(request, 'employer_search/employer_search_t.html')
+
+def employer_search_u(request):
+    return render(request, 'employer_search/employer_search_u.html')
+
+def employer_search_v(request):
+    return render(request, 'employer_search/employer_search_v.html')
+
+def employer_search_w(request):
+    return render(request, 'employer_search/employer_search_w.html')
+
+def employer_search_x(request):
+    return render(request, 'employer_search/employer_search_x.html')
+
+def employer_search_y(request):
+    return render(request, 'employer_search/employer_search_y.html')
+
+def employer_search_z(request):
+    return render(request, 'employer_search/employer_search_z.html')
